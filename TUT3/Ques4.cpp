048c6a3d7a45dcac0d11adf0e13407af60217af1
Tanish Garg <gargtanish118@gmail.com>
2025-09-06 22:16:33 +0530

    TUT3 DSA

--- TUT3/Ques4.cpp
+++ TUT3/Ques4.cpp
@@ -1,69 +1,3 @@
-// #include <iostream>
-// #include <stack>
-// #include <cctype>  // for isalnum
-// using namespace std;
-// // Function to return precedence of operators
-// int precedence(char op) {  // Higher value means higher precedence, returns precedence level of operator
-//     if(op == '^') return 3;
-//     if(op == '*' || op == '/') return 2;
-//     if(op == '+' || op == '-') return 1;
-//     else return -1; // for non-operators
-//     return 0; 
-// }
-// // Function to check associativity (only ^ is right-associative)
-// bool isRightAssociative(char op) {
-//     return (op == '^');
-// }
-// // Function to convert Infix -> Postfix
-// string infixToPostfix(string infix) {
-//     stack<char> st;
-//     string postfix = "";
-
-//     for(char c : infix) {
-//         if(isalnum(c)) {
-//             // Operand → directly add to postfix
-//             postfix += c;
-//         }
-//         else if(c == '(') {
-//             st.push(c);
-//         }
-//         else if(c == ')') {
-//             // Pop until '(' is found
-//             while(!st.empty() && st.top() != '(') {
-//                 postfix += st.top();
-//                 st.pop();
-//             }
-//             st.pop(); // remove '('
-//         }
-//         else {
-//             // Operator
-//             while(!st.empty() && precedence(st.top()) > precedence(c) ||
-//                  (!st.empty() && precedence(st.top()) == precedence(c) && !isRightAssociative(c))) {
-//                 postfix += st.top();
-//                 st.pop();
-//             }
-//             st.push(c);
-//         }
-//     }
-//     // Pop remaining operators
-//     while(!st.empty()) {
-//         postfix += st.top();
-//         st.pop();
-//     }
-//     return postfix;
-// }
-// int main() {
-//     string infix;
-//     cout << "Enter Infix Expression: ";
-//     cin >> infix;
-//     string postfix = infixToPostfix(infix);
-//     cout << "Postfix Expression: " << postfix << endl;
-
-//     return 0;
-// }
-
-
-
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -74,7 +8,6 @@ int prec(char c) {
     if(c == '+' || c == '-') return 1;
     else return -1;
 }
-
 string infixToPostfix(string s) {
     stack<char> st;
     string res;
@@ -108,7 +41,6 @@ string infixToPostfix(string s) {
     }
     return res;
 }
-
 int main(){
     string s;
     cout<<"Enter Infix Expression : ";
@@ -119,3 +51,4 @@ int main(){
 
     return 0;
 }
+
